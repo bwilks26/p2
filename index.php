58ea3d7173b3388432dc1e9e913d8d2fa14e8c01
bwilks26 <bryan@freeformagency.com>
2018-02-20 00:56:24 -0600

    styling changes to files

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require 'logic.php';
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Tip Calculator</title>
+    <title>Bill Splitter</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
@@ -23,27 +23,28 @@ require 'logic.php';
         <form method='GET' action='index.php'>
             <div class='row-fluid'>
                 <label>Split how many ways?</label>
-                <input type='text' name='splitTerm' value='<?=sanitize($splitTerm)?>'/>
+                <input type='text' name='splitTerm' value='<?= sanitize($splitTerm) ?>'/>
             </div>
             <div class='row-fluid'>
                 <label>How much was the tab?</label>
-                <input type='text' name='billAmount' value='<?=sanitize($billAmount)?>'/>
+                <input type='text' name='billAmount' value='<?= sanitize($billAmount) ?>'/>
             </div>
 
-            <div class=''row-fluid'>
+            <div class='row-fluid'>
                 <label>How was the the service?</label>
                 <select name='tipAmount'>
-                    <option value='0' <?php if ($tipAmount == 0) echo 'selected'?>>Choose tip...</option>
-                    <option value='15' <?php if ($tipAmount == 15) echo 'selected'?>>Poor (15%)</option>
-                    <option value='18' <?php if ($tipAmount == 18) echo 'selected'?>>Good (18%)</option>
-                    <option value='20' <?php if ($tipAmount == 20) echo 'selected'?>>Excellent (20%)</option>
-                    <option value='25' <?php if ($tipAmount == 25) echo 'selected'?>>Outstanding (25%)</option>
+                    <!-- echo selected on appropriate tip amount selected for refresh -->
+                    <option value='0' <?php if ($tipAmount == 0) echo 'selected' ?>>Choose tip...</option>
+                    <option value='15' <?php if ($tipAmount == 15) echo 'selected' ?>>Poor (15%)</option>
+                    <option value='18' <?php if ($tipAmount == 18) echo 'selected' ?>>Good (18%)</option>
+                    <option value='20' <?php if ($tipAmount == 20) echo 'selected' ?>>Excellent (20%)</option>
+                    <option value='25' <?php if ($tipAmount == 25) echo 'selected' ?>>Outstanding (25%)</option>
                 </select>
             </div>
 
             <div class='row-fluid'>
                 <label>Round up?</label>
-                <input type='checkbox' name='roundUp' <?=($roundUp) ? 'checked' : ''?> />
+                <input type='checkbox' name='roundUp' <?= ($roundUp) ? 'checked' : '' ?> />
             </div>
 
             <input type='submit' value='Calculate'>
@@ -51,20 +52,19 @@ require 'logic.php';
     </div>
 </div>
 
-
-
-
+<!-- show the correct alert upon first visiting page or submitting form -->
 <?php if (($isSubmitted) && ($form->hasErrors)) : ?>
     <div class='alert alert-danger' role='alert'>
         <ul>
             <?php foreach ($errors as $error) : ?>
-                <li><?=$error ?></li>
+                <li><?= $error ?></li>
             <?php endforeach; ?>
     </div>
 <?php elseif (($isSubmitted) && (!$form->hasErrors)): ?>
     <div class='alert alert-success' role='alert'>
-        <p>You split <em>$<?=sanitize($billAmount)?></em> into <em><?=sanitize($splitTerm)?></em> ways with <em><?=sanitize($tipAmount) . '% tip'?></em>.</p>
-        <p>Bill split: Everyone owes <em>$<?=sanitize($result)?> each</em></p>
+        <p>You split <em>$<?= sanitize($billAmount) ?></em> into <em><?= sanitize($splitTerm) ?></em> ways with
+            <em><?= sanitize($tipAmount) . '% tip' ?></em>.</p>
+        <p>Bill split: Everyone owes <em>$<?= sanitize($result) ?> each</em></p>
     </div>
 <?php else : ?>
     <div class='alert alert-info' role='alert'>
--- logic.php
+++ logic.php
@@ -1,21 +1,21 @@
 <?php
 
-require ('Form.php');
+require('Form.php');
 
 use DWA\Form;
 
 $form = new Form($_GET);
 
-//$splitTerm = (has($form->get('splitTerm'))) ? $form->get('splitTerm') : '';
-$splitTerm = $form->get('splitTerm', $default = '5');
-$billAmount = $form->get('billAmount', $default = '10');
-$tipAmount = $form->get('tipAmount');
-$roundUp = $form->has('roundUp');
-
 $isSubmitted = $form->isSubmitted();
 
-
 if ($isSubmitted) {
+    // get form values from GET request
+    $splitTerm = $form->get('splitTerm', $default = '5');
+    $billAmount = $form->get('billAmount', $default = '10');
+    $tipAmount = $form->get('tipAmount');
+    $roundUp = $form->has('roundUp');
+
+    // set our rules for form validation
     $errors = $form->validate(
         [
             'splitTerm' => 'required|numeric',
@@ -23,18 +23,20 @@ if ($isSubmitted) {
         ]
     );
 
+    // calculate correct result if form has no errors
     if (!$form->hasErrors) {
-        if($tipAmount) {
+        if ($tipAmount) {
             $result = ($billAmount + ($billAmount * ($tipAmount / 100))) / $splitTerm;
-        }
-        else {
+        } else {
             $result = $billAmount / $splitTerm;
         }
 
+        // round up by taking the ceiling of number if selected
         if ($roundUp) {
             $result = ceil($result);
         }
 
+        // format result to be more user-friendly
         $result = number_format($result, 2, '.', ',');
     }
 }
